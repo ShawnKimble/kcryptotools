4ae0f86779bcb24760fa299defb7a59ff0214dfb
ukurokawa <ukurokawa@pavement.(none)>
2014-11-09 14:34:44 -0500

    restructured folders for setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kcryptotools',
+      version='0.0',
+      description='Kay\'s Crypto Tools',
+      author='Umpei Kay Kurokawa',
+      author_email='umpeikurokawa@gmail.com',
+      url='https://github.com/kaykurokawa/kcryptotools',
+      packages=['kcryptotools']
+     )
